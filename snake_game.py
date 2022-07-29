98cee6ac93d32588d2cd657ef63051f585901629
Sushovit Shah <shahsushovit434@gmail.comgit config --global user.email shahsushovit434@gmail.com>
2022-07-29 16:24:52 +0545

    Setting Food and pen

--- snake_game.py
+++ snake_game.py
@@ -10,12 +10,31 @@ wnd.tracer(0)
 #snake head
 head=turtle.Turtle()
 head.speed(0)
-head.shape("square")
+head.shape("circle")
 head.color("black")
-head.penup
+head.penup()
 head.goto(0,0)
 head.direction="stop"
 
 #snake food
 food = turtle.Turtle()
+food.speed(0)
+food.shape("triangle")
+food.color("red")
+food.penup()
 food.goto(0,100)
+
+segments=[]
+
+# Pen
+pen=turtle.Turtle
+pen.speed(0)
+pen.shape("oval")
+pen.color("brown")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,250)
+pen.write("Score: 0  High Score: 0", align="centre", font=("Courier", 22, "normal"))
+
+
+

64493121ed75496b3beac6fe29703c20b1053467
Sushovit Shah <shahsushovit434@gmail.comgit config --global user.email shahsushovit434@gmail.com>
2022-07-29 16:38:14 +0545

    Defining the movement and setting the key-bindings

--- snake_game.py
+++ snake_game.py
@@ -36,5 +36,39 @@ pen.hideturtle()
 pen.goto(0,250)
 pen.write("Score: 0  High Score: 0", align="centre", font=("Courier", 22, "normal"))
 
+#Functions
+def _go_up():
+    if head.direction !="down":
+        head.direction = "up"
 
+def _go_down():
+    if head.direction != "up":
+        head.direction = "down"
 
+def _go_left():
+    if head.direction != "right":
+        head.direction = "left"
+
+def _go_right():
+    if head.direction != "left":
+        head.direction = "right"
+
+def _move_():
+    if head.direction() == "up":
+        y = head.ycor()
+        head.sety(y +20)
+
+    if head.direction() =="down":
+        y = head.ycor()
+        head.setx(y - 20)
+
+    if head.direction() =="left":
+        x = head.xcor()
+        head.setx(x - 20)
+
+    if head.direction() =="right":
+        x = head.xcor
+        head.setx(x + 20)
+
+ #Jey-bindings for the movements of snake   
+     

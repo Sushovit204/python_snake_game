51be4ebf081aa95fc38cff1b23a4b824a99cd423
Sushovit Shah <shahsushovit434@gmail.comgit config --global user.email shahsushovit434@gmail.com>
2022-07-29 15:55:34 +0545

    Setting up Head and food turtle

--- snake_game.py
+++ snake_game.py
@@ -6,3 +6,16 @@ wnd.title("Snake Game by @Sushovit204")
 wnd.bgcolor("blue")
 wnd.setup(width=500, height=500)
 wnd.tracer(0)
+
+#snake head
+head=turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black")
+head.penup
+head.goto(0,0)
+head.direction="stop"
+
+#snake food
+food = turtle.Turtle()
+food.goto(0,100)
